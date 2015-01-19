e47a620e3dd38f9274dd801ef4a6c1dbafccb4a9
Bryan Johnson <bryanj@pcsoln.com>
2015-01-19 17:33:21 -0600

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'my-app'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures my-app'
+long_description 'Installs/Configures my-app'
+version          '0.1.0'
+
