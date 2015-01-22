d6b33c9f71eb5347c5f1a0145ce95d838c1e3d37
Bryan Johnson <bryanj@pcsoln.com>
2015-01-22 10:34:31 -0600

    Added tomcat support. Created rubocop config file

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,11 @@
 # Recipe:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
+
+# Install the service
+
+include_recipe 'tomcat'
+
+httpd_service 'default' do
+  action [:create, :start]
+end
