4c7b508fc9bfd811fde2522af2c3746832095657
Bryan Johnson <bryanj@pcsoln.com>
2015-01-23 23:19:59 -0600

    add java and apache

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,11 +6,27 @@
 
 # Install the service
 
+
+include_recipe 'java'
+
+node.default["tomcat"]["ssl_port"] = nil
 include_recipe 'tomcat'
 
 war = "#{node['tomcat']['webapp_dir']}/#{node['sample-app']['app-name']}.war"
 remote_file war do
   source node['sample-app']['url']
+end
+
+
+%w(proxy proxy_ajp).each do |apache_module|
+  httpd_module apache_module do
+    action :create
+  end
+end
+
+httpd_config node['sample-app']['app-name'] do
+  source "#{node['sample-app']['app-name']}.conf.erb"
+  action :create
 end
 
 httpd_service 'default' do
